218d943e9765a1d305c568856c1fe7212787d55b
Dan Kogai <dankogai+github@gmail.com>
2016-02-09 01:59:57 +0900

    mv poextraops.swift poxtraops.swift

--- OSX.playground/Pages/Welcome.xcplaygroundpage/Contents.swift
+++ OSX.playground/Pages/Welcome.xcplaygroundpage/Contents.swift
@@ -1,5 +1,6 @@
 import Cocoa    // this is an OSX playground
 //: Playground - noun: a place where people can play
+
 let bn = BigInt(2) ** 128
 let bq = BigInt(1).over(bn)
 let bz = bq + bq.i
@@ -15,3 +16,5 @@ UInt.precision
 Double.precision
 Float.precision
 Int8.precision
+
+//: [Next](@next)
---
+++ iOS.playground/Sources/poxtraops.swift
@@ -0,0 +1,1 @@
+../../pons/poxtraops.swift
