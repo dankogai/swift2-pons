d5c01cef8caac10c6df1b94205ca6943f4d3b7cb
Dan Kogai <dankogai+github@gmail.com>
2016-02-08 13:00:19 +0900

    PORational complete except for operators

--- pons/porational.swift
+++ pons/porational.swift
@@ -33,7 +33,82 @@ public extension POInt {
         return self.toRational(dominator)
     }
 }
-public struct Rational<U:POUInt> : POReal {
+public protocol PORational : POReal {
+    typealias UIntType:POUInt
+    var sgn:Bool { get set }
+    var num:UIntType { get set }
+    var den:UIntType { get set }
+    init(_:Bool, _:UIntType, _:UIntType, isNormal:Bool)
+}
+public extension PORational {
+    public func toIntMax()->IntMax {
+        return ((sgn ? -1 : 1) * num / den).toIntMax()
+    }
+    public func toDouble()->Double {
+        return (sgn ? -1 : 1) * Double(num.toUIntMax()) / Double(den.toUIntMax())
+    }
+    public var isSignMinus:Bool { return sgn }
+    public var isInfinite:Bool  { return den == 0 && num != 0 }
+    public static var infinity:Self  { return Self(false, 1, 0, isNormal:true) }
+    public var isNaN:Bool       { return den == 0 && num == 0 }
+    public static var NaN:Self  { return Self(false, 0, 0, isNormal:true) }
+    public var isZero:Bool      { return den != 0 && num == 0 }
+    public static var zero:Self  { return Self(false, 0, 1, isNormal:true) }
+    public var description:String {
+        let s = sgn ? "-" : ""
+        return "\(s)(\(num)/\(den))"
+    }
+    public var hashValue:Int {
+        let bits = sizeof(Int) * 4
+        return (sgn ? -1 : 1) * (((num.hashValue >> bits) << bits) | (den.hashValue >> bits))
+    }
+    public static func multiplyWithOverflow(lhs:Self, _ rhs:Self)->(Self, overflow:Bool) {
+        var ln = lhs.num, ld = lhs.den, rn = rhs.num, rd = rhs.den;
+        let gn = UIntType.gcd(ln, rd), gd = UIntType.gcd(ld, rn);
+        ln /= gn; rn /= gd;
+        ld /= gd; rd /= gn;
+        let (n, nof) = UIntType.multiplyWithOverflow(ln, rn)
+        let (d, dof) = UIntType.multiplyWithOverflow(ld, rd)
+        return (Self(Bool.xor(lhs.sgn, rhs.sgn), n, d, isNormal:true), overflow: nof || dof)
+    }
+    public var reciprocal:Self {
+        var newValue = self
+        (newValue.num, newValue.den) = (den, num)
+        return newValue
+    }
+    public static func divideWithOverflow(lhs:Self, _ rhs:Self)->(Self, overflow:Bool) {
+        return multiplyWithOverflow(lhs, rhs.reciprocal)
+    }
+    public static func addWithOverflow(lhs:Self, _ rhs:Self)->(Self, overflow:Bool) {
+        if lhs.den == rhs.den {
+            let (n, nof) = Bool.xor(lhs.sgn, rhs.sgn)
+                ? lhs.num < rhs.num ? UIntType.subtractWithOverflow(rhs.num, lhs.num)
+                    : UIntType.subtractWithOverflow(lhs.num, rhs.num)
+                    : UIntType.addWithOverflow(lhs.num, rhs.num)
+            var result = lhs
+            result.sgn = lhs.num < rhs.num ? rhs.sgn: lhs.sgn
+            result.num = n
+            return (result, overflow:nof)
+        } else {
+            var l = lhs, r = rhs
+            var (o0, o1, o2, o3) = (false, false, false, false)
+            var d = UIntType(0), fin = lhs
+            // print("add:", l, r)
+            let g = UIntType.gcd(lhs.den, rhs.den)
+            (d, o0) = UIntType.multiplyWithOverflow(l.den, r.den / g)
+            (l.num, o1) = UIntType.multiplyWithOverflow(l.num, r.den / g)
+            (r.num, o2) = UIntType.multiplyWithOverflow(r.num, l.den / g)
+            l.den = d ; r.den = d
+            // print("add:", l, r)
+            (fin, o3) = addWithOverflow(l, r)
+            return (fin, overflow: o0 || o1 || o2 || o3)
+        }
+    }
+    public static func subtractWithOverflow(lhs:Self, _ rhs:Self)->(Self, overflow:Bool) {
+        return addWithOverflow(lhs, -rhs)
+    }
+}
+public struct Rational<U:POUInt> : PORational {
     public typealias UIntType = U
     public var sgn:Bool = false
     public var num:U = 0
@@ -55,7 +130,7 @@ public struct Rational<U:POUInt> : POReal {
     }
     public init(_ r:Double) {
         let (m, e) = Double.frexp(r)
-        let n = unsafeBitCast(m, UInt64.self) | UInt64(0x000fffffffffffff) | (1 << 52)
+        let n = unsafeBitCast(m, UInt64.self) | UInt64(0x001f_ffff_ffff_ffff)
         let d = UInt64(1 << (e - 1))
         self.init(r.isSignMinus, UIntType(n), UIntType(d))
     }
@@ -63,69 +138,6 @@ public struct Rational<U:POUInt> : POReal {
     public typealias IntegerLiteralType = Int.IntegerLiteralType
     public init(integerLiteral:IntegerLiteralType) {
         self.init(integerLiteral)
-    }
-    public func toIntMax()->IntMax {
-        return ((sgn ? -1 : 1) * num / den).toIntMax()
-    }
-    public func toDouble()->Double {
-        return (sgn ? -1 : 1) * Double(num.toUIntMax()) / Double(den.toUIntMax())
-    }
-    public var description:String {
-        let s = sgn ? "-" : ""
-        return "\(s)(\(num)/\(den))"
-    }
-    public var hashValue:Int {
-        let bits = sizeof(Int) * 4
-        return (sgn ? -1 : 1) * (((num.hashValue >> bits) << bits) | (den.hashValue >> bits))
-    }
-    public var isSignMinus:Bool { return sgn }
-    public var isInfinite:Bool  { return den == 0 && num != 0 }
-    public static var infinity:Rational<U>  { return Rational(false, 1, 0, isNormal:true) }
-    public var isNaN:Bool       { return den == 0 && num == 0 }
-    public static var NaN:Rational<U>  { return Rational(false, 0, 0, isNormal:true) }
-    public var isZero:Bool      { return den != 0 && num == 0 }
-    public static var zero:Rational<U>  { return Rational(false, 0, 1, isNormal:true) }
-    public static func multiplyWithOverflow(lhs:Rational<U>, _ rhs:Rational<U>)->(Rational<U>, overflow:Bool) {
-        var ln = lhs.num, ld = lhs.den, rn = rhs.num, rd = rhs.den;
-        let gn = UIntType.gcd(ln, rd), gd = UIntType.gcd(ld, rn);
-        ln /= gn; rn /= gd;
-        ld /= gd; rd /= gn;
-        let (n, nof) = U.multiplyWithOverflow(ln, rn)
-        let (d, dof) = U.multiplyWithOverflow(ld, rd)
-        return (Rational(Bool.xor(lhs.sgn, rhs.sgn), n, d, isNormal:true), overflow: nof || dof)
-    }
-    public var reciprocal:Rational<U> {
-        var newValue = self
-        (newValue.num, newValue.den) = (den, num)
-        return newValue
-    }
-    public static func divideWithOverflow(lhs:Rational<U>, _ rhs:Rational<U>)->(Rational<U>, overflow:Bool) {
-        return multiplyWithOverflow(lhs, rhs.reciprocal)
-    }
-    public static func addWithOverflow(lhs:Rational<U>, _ rhs:Rational<U>)->(Rational<U>, overflow:Bool) {
-        if lhs.den == rhs.den {
-            let (n, nof) = Bool.xor(lhs.sgn, rhs.sgn)
-                ? lhs.num < rhs.num ? UIntType.subtractWithOverflow(rhs.num, lhs.num)
-                    : UIntType.subtractWithOverflow(lhs.num, rhs.num)
-                : UIntType.addWithOverflow(lhs.num, rhs.num)
-            return (Rational(lhs.num < rhs.num ? rhs.sgn: lhs.sgn, n, lhs.den), overflow:nof)
-        } else {
-            var l = lhs, r = rhs
-            var (o0, o1, o2, o3) = (false, false, false, false)
-            var d = U(0), fin = lhs
-            // print("add:", l, r)
-            let g = U.gcd(lhs.den, rhs.den)
-            (d, o0) = U.multiplyWithOverflow(l.den, r.den / g)
-            (l.num, o1) = U.multiplyWithOverflow(l.num, r.den / g)
-            (r.num, o2) = U.multiplyWithOverflow(r.num, l.den / g)
-            l.den = d ; r.den = d
-            // print("add:", l, r)
-            (fin, o3) = addWithOverflow(l, r)
-            return (fin, overflow: o0 || o1 || o2 || o3)
-        }
-    }
-    public static func subtractWithOverflow(lhs:Rational<U>, _ rhs:Rational<U>)->(Rational<U>, overflow:Bool) {
-        return addWithOverflow(lhs, -rhs)
     }
 }
 public func ==<U:POUInt>(lhs:Rational<U>, rhs:Rational<U>) -> Bool {
