40d3bf5e82559ce151072116a0fdbdcda815d9c1
Dan Kogai <dankogai+github@gmail.com>
2016-02-15 21:53:13 +0900

    directory cleanups

---
+++ osx/OSX.playground/Pages/Rational.xcplaygroundpage/Contents.swift
@@ -0,0 +1,14 @@
+//: [Previous](@previous)
+import PONS
+
+BigRat(42.195)
+BigInt(42195).over(1000)
+(BigRat(42.195) - BigInt(42195).over(1000)).toDouble().toFPString(16)
+BigRat(42.195).toFPString()
+BigRat(42.195).toFPString(16)
+BigRat(42.195).toFPString(10,places:4)
+BigRat(42.195).toFPString(10,places:3)
+BigRat(42.195).toFPString(10,places:2)
+BigRat(42.195).toFPString(10,places:1)
+
+//: [Next](@next)
